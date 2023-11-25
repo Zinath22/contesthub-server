666d9987c6b7782852b1d7b06548a32e67ae41dc
Zinath22 <Zinath22@users.noreply.github.com>
2023-11-26 01:22:56 +0600

    payment confirm added

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const cors = require('cors');
-require ('dotenv').config();
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 
@@ -30,28 +31,69 @@ async function run() {
 
 
     const contestCollection = client.db("contestDb").collection("contest");
+    const paymentCollection = client.db("contestDb").collection("payments");
+    // const userCollection = client.db("contestDb").collection("payments");
+    
 
-    app.get('/contest', async(req, res) => {
+    app.get('/contest', async (req, res) => {
       const result = await contestCollection.find().toArray();
       res.send(result);
     });
 
     // details 
-     app.get('/contest/:id', async(req, res) => {
+    app.get('/contest/:id', async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await contestCollection.findOne(query);
       res.send(result);
-     });
+    });
 
-    //  app.get('/allFood/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) }
-    //   const result = await foodCollection.findOne(query);
-    //   res.send(result);
-    // });
+    //  register
+    app.get('/register', async (req, res) => {
+      const result = await surveyCollection.find().toArray();
+      res.send(result);
+    });
 
-    
+    app.post('/register', async (req, res) => {
+      const item = req.body;
+      const result = await surveyCollection.insertOne(item);
+      res.send(result);
+    });
+    //  payment 
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      console.log('amount', amount);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
+
+    //  send payment database 
+    app.post('/payments', async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+
+      console.log('payment info', payment);
+      const query = {
+        _id: {
+          $in: payment.contestIds.map(id => new ObjectId(id))
+        }
+      };
+
+      // const deleteresult = await contestCollection.deleteMany(query);
+
+      res.send({ paymentResult })
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -63,10 +105,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req, res) =>{
-    res.send('contest is running')
+app.get('/', (req, res) => {
+  res.send('contest is running')
 })
 
 app.listen(port, () => {
-    console.log(`Contest is running on port ${port}`);
+  console.log(`Contest is running on port ${port}`);
 })
